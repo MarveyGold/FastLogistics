214d2f27b75e3c66ba526dd79ae682692a1759e4
MarveyGold <145897274+MarveyGold@users.noreply.github.com>
2025-03-10 12:49:59 +0100

    Update and rename Button.js to Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import styles from "@/styles/Header.module.css";  
 
-export default function Button() {
+export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
