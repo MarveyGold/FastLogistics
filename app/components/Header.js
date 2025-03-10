8d1dbcb1499b891db767459538a9c31970852929
MarveyGold <145897274+MarveyGold@users.noreply.github.com>
2025-03-10 12:09:11 +0100

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import styles from "@/styles/Header.module.css";
+import Button from "./components/Button"
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -14,7 +15,7 @@ export default function Header() {
          <h2 className={styles.l} >F ast Logistics</h2>
             <i >excellence inspired</i>
         </div>
-        
+        <Button />
         <nav className={`${styles.nav} ${menuOpen ? styles.active : '' }` } >
             <ul className={styles.ul} >
                 <li> <Link href="#home" className={styles.u} >Home</Link> </li>
