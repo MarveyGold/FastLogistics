803f5b2d36151a1df3ed71d68dde14bf0d696012
MarveyGold <145897274+MarveyGold@users.noreply.github.com>
2025-03-10 12:14:50 +0100

    Update Button.js

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,6 +1,9 @@
 'use client';
 import styles from "@/styles/Header.module.css";
 export default function Button(){
+  const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
   return(
     <button className={styles.menuToggle} onClick={toggleMenu} aria-label="Navigation Menu" >☰</button>
   )
