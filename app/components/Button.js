e01c364509b26c230f9da8dec05876af83205118
MarveyGold <145897274+MarveyGold@users.noreply.github.com>
2025-03-10 12:10:56 +0100

    Update Button.js

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,4 +1,5 @@
 'use client';
+import styles from "@/styles/Header.module.css"
 export default function Button(){
   return(
     <button className={styles.menuToggle} onClick={toggleMenu} aria-label="Navigation Menu" >☰</button>
